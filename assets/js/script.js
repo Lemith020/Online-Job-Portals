07382b71d417a53e239906aff2ae3f9195594f8a
NANDITHA W.D.L <lemithnanditha41@gmail.com>
2026-09-05 21:56:56 +0530

    Delete assets/js/script.js

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// -----------------------------------------------------------------
-// script.js - Global JavaScript
-// -----------------------------------------------------------------
-document.addEventListener("DOMContentLoaded", function () {
-    // TODO: form validation, navbar toggle, AJAX calls, etc.
-    console.log("Job Portal script loaded");
-});
